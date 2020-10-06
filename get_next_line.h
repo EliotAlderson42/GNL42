72312b3ece14339c581ec24c814b3fe57e427171
EliotAlderson <eliotalderson@Mr.Robot>
2020-10-06 10:22:28 +0200

    real

--- get_next_line.h
+++ get_next_line.h
@@ -3,18 +3,18 @@
 /*                                                        :::      ::::::::   */
 /*   get_next_line.h                                    :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: yhouari <yhouari@student.42.fr>                      +#+  +:+       +#+        */
+/*   By: yhouari <yhouari@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2020/10/05 04:10:29 by user42            #+#    #+#             */
-/*   Updated: 2020/10/05 04:22:02 by user42           ###   ########.fr       */
+/*   Created: 2020/10/06 10:21:50 by yhouari           #+#    #+#             */
+/*   Updated: 2020/10/06 10:21:53 by yhouari          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef GET_NEXT_LINE_H
-#define GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
 
-#include <unistd.h>
-#include <stdlib.h>
+# include <unistd.h>
+# include <stdlib.h>
 
 char	*ft_next_line(char *str);
 char	*ft_line(char *str);
